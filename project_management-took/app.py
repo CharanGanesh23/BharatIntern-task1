34e215646eafa8a33040ed068683de1e7efffa5e
CharanGanesh23 <139037363+CharanGanesh23@users.noreply.github.com>
2023-07-28 19:53:53 +0530

    Update app.py

--- project_management-took/app.py
+++ project_management-took/app.py
@@ -1,26 +1,15 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_session import Session
-
-# FLASK_APP = app.py
-# FLASK_ENV = development
-# FLASK_DEBUG = 0
-
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'
-
 Session(app)
-
 socketio = SocketIO(app, manage_session=False)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-
-
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
@@ -34,21 +23,15 @@ def chat():
             return render_template('chat.html', session=session)
         else:
             return redirect(url_for('index'))
-
-
 @socketio.on('join', namespace='/chat')
 def join():
     room = session.get('room')
     join_room(room)
     emit('status', {'msg': session.get('username') + ' has entered the room.'}, room=room)
-
-
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
     emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
-
-
 @socketio.on('left', namespace='/chat')
 def left(message):
     room = session.get('room')
@@ -56,8 +39,6 @@ def left(message):
     leave_room(room)
     session.clear()
     emit('status', {'msg': username + ' has left the room.'}, room=room)
-
-
 if __name__ == '__main__':
     socketio.run(app)
 
